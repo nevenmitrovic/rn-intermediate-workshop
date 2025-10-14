f58e41e2499e2901a8e064ffe4d5b03cdbbaf8e1
neven <nevenmitrovic4@gmail.com>
2025-10-14 10:55:28 +0200

    add layout groups

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-import { theme } from '../theme';
+import { theme } from '@/theme';
 
 export default function App() {
 	return (
--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,6 +1,6 @@
 import { Text, View, StyleSheet } from 'react-native';
 
-import { theme } from '../theme';
+import { theme } from '@/theme';
 
 export default function ProfileScreen() {
 	return (
