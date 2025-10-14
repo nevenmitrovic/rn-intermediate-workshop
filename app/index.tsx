f996e7d1467e31b2fca8a17c491a382430f19652
neven <nevenmitrovic4@gmail.com>
2025-10-14 10:35:36 +0200

    add bottom tabs

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { theme } from '../theme';
+
 export default function App() {
 	return (
 		<View style={styles.container}>
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#fff',
+		backgroundColor: theme.colorWhite,
 		alignItems: 'center',
 		justifyContent: 'center',
 	},
